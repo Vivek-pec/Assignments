486506f12df4cbe19f818d9ce1dbaaf4d94cf1c4
Vivek-pec <97089305+Vivek-pec@users.noreply.github.com>
2022-01-26 22:38:49 +0530

    Add files via upload

---
+++ Assignment 2/asg2_21105011_OUTPUT.py
@@ -0,0 +1,45 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\Lenovo\Desktop\asg2_21105011.py ==============
+                     ASSIGNMENT 2
+
+QUESTION 1 
+Given string is  :   Python is a case sensitive language
+a.  Lenth of the string is :   35
+b.  Reversed string is :  egaugnal evitisnes esac a si nohtyP
+c. 'a case sensitive ' stored in a new string 
+d. String after replacement of 'a case sensitive' with 'object oriented' :  Python is object oriented language
+e. Index of 'a' in the given input string : 10
+f. After removing white spaces from the input string :  Pythonisacasesensitivelanguage
+
+QUESTION 2
+
+ Hey, Vivek Jaiswal Here!
+ My SID is 21105011
+ I am from Electronics and Communication Engineering  department and my CGPA is 9.8 
+
+QUESTION 3
+
+a.  a&b :  8
+b.  a|b :  58
+c.  a^b :  50
+d.  a<<2 :  224 
+     b<<2 :  40
+d.  a>>2 :  14 
+     b>>4 :  0
+
+QUESTION 4
+
+Enter three numbers separated by space :   33 73 94
+Greatest of three numbers you entered is :  94
+
+QUESTION 5
+
+Enter a string to find if it contains 'name' or not  :  My name is Simba.
+Yes
+
+QUESTION 6
+
+Enter three sides  separated by space to check whether we can form a triangle with them or not :  3 4 5
+Yes
